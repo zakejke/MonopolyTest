e95a5915e501870815c09b3f42615bfddaee7455
Alex <zakejke@mail.ru>
2024-11-12 20:10:07 +0300

    Initial commit

---
+++ WarehouseApp/FileManager.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WarehouseApp
+{
+    public static class FileManager
+    {
+        public static void SavePalletsToFile(List<Pallet> pallets, string filePath)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(pallets, Formatting.Indented);
+                File.WriteAllText(filePath, json);
+                Console.WriteLine("Данные успешно сохранены в файл.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении данных: {ex.Message}");
+            }
+        }
+
+        public static List<Pallet> LoadPalletsFromFile(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var json = File.ReadAllText(filePath);
+                    var pallets = JsonConvert.DeserializeObject<List<Pallet>>(json);
+                    Console.WriteLine("Данные успешно загружены из файла.");
+                    return pallets;
+                }
+                else
+                {
+                    Console.WriteLine("Файл не найден.");
+                    return new List<Pallet>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке данных: {ex.Message}");
+                return new List<Pallet>();
+            }
+        }
+    }
+}
---
+++ WarehouseApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WarehouseApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pallets = DataGenerator.GeneratePallets(5, 10);
+
+            WarehouseDisplay.DisplayPalletsByExpiration(pallets);
+
+            WarehouseDisplay.DisplayTopPalletsByLongestBoxExpiration(pallets);
+
+            AddBoxToPallet(pallets);
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+
+        static void AddBoxToPallet(List<Pallet> pallets)
+        {
+            Console.WriteLine("Доступные паллеты (ID):");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"ID: {pallet.Id}");
+            }
+
+            Console.Write("Введите ID паллета для добавления коробки (GUID): ");
+            var palletIdInput = Console.ReadLine();
+
+            if (Guid.TryParse(palletIdInput, out Guid palletId))
+            {
+                var pallet = pallets.FirstOrDefault(p => p.Id == palletId);
+                if (pallet == null)
+                {
+                    Console.WriteLine("Паллет с таким ID не найден.");
+                    return;
+                }
+
+                // Ввод данных для коробки
+                Console.Write("Введите ширину коробки (см): ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите глубину коробки (см): ");
+                int depth = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите высоту коробки (см): ");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите вес коробки (кг): ");
+                double weight = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите срок годности коробки (дд.мм.гггг): ");
+                DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Введите дату производства коробки (дд.мм.гггг): ");
+                DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+                Box newBox = new Box(width, depth, height, weight, expirationDate, manufactureDate);
+                pallet.AddBox(newBox);
+                Console.WriteLine("Коробка добавлена к паллету.");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный формат GUID.");
+            }
+        }
+    }
+}
